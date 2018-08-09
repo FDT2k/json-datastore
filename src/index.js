342d1cd75068e65a7a11c0a3a048af6b5914b790
Steve Randy Tantra <steve.randy@gmail.com>
2018-08-09 12:53:12 +0800

    Add eslint

--- src/index.js
+++ src/index.js
@@ -1,142 +1,156 @@
-let fs = require("fs")
-let path = require("path")
-let pify = require("pify")
-let mkdirp = require("mkdirp")
-let writeFileAtomic = require("write-file-atomic")
+let fs = require("fs");
+let path = require("path");
+let pify = require("pify");
+let mkdirp = require("mkdirp");
+let writeFileAtomic = require("write-file-atomic");
 
 // Promisify fs functions
-fs = pify(fs)
-fs.mkdirp = pify(mkdirp)
-writeFileAtomic = pify(writeFileAtomic)
+fs = pify(fs);
+fs.mkdirp = pify(mkdirp);
+writeFileAtomic = pify(writeFileAtomic);
+
+function generateDefaultId() {
+  let timestamp = (new Date().getTime() / 1000 | 0).toString(16);
+  return timestamp + "xxxxxxxxxxxxxxxx".replace(/[x]/g, () => {
+    return (Math.random() * 16 | 0).toString(16);
+  }).toLowerCase();
+}
 
 let readJsonFile = (file, options) => {
   return new Promise((resolve, reject) => {
     fs.readFile(file, options).then(data => {
-      try { data = JSON.parse(data) }
-      catch (err) { return reject(err) }
-      resolve(data)
-    }).catch(error => reject(error))
-  })
-}
+      try {
+        data = JSON.parse(data);
+      }
+      catch (error) {
+        return reject(error);
+      }
+      resolve(data);
+    }).catch((error) => {
+      reject(error);
+    });
+  });
+};
 
 let writeJsonFile = (file, data, options) => {
   return new Promise((resolve, reject) => {
-    data = JSON.stringify(data)
-    writeFileAtomic(file, data, options)
-    .then(data => resolve())
-    .catch(error => reject(error))
-  })
-}
+    data = JSON.stringify(data);
+    writeFileAtomic(file, data, options).then((data) => {
+      resolve();
+    }).catch((error) => {
+      reject(error);
+    });
+  });
+};
 
 let filterObjects = (objects, query) => {
-  let queryKeys = Object.keys(query)
+  let queryKeys = Object.keys(query);
   return objects.filter(object => {
-    let matches = 0
-    let objectKeys = Object.keys(object)
-    if (object == null || objectKeys.length < 1) return false
+    let matches = 0;
+    let objectKeys = Object.keys(object);
+    if (object == null || objectKeys.length < 1) return false;
     queryKeys.forEach(key => {
-      if (object[key] === queryKeys[key]) matches++
-    })
-    return queryKeys.length === matches
-  })
-}
+      if (object[key] === queryKeys[key]) matches++;
+    });
+    return queryKeys.length === matches;
+  });
+};
 
 let sortObjects = (objects, sortKey, options = {}) => {
-  let returnValues = [1, -1]
+  let returnValues = [1, -1];
   if (options.descending === true) {
-    returnValues = returnValues.reverse()
+    returnValues = returnValues.reverse();
   }
   return objects.sort((x, y) => {
-    if (x[sortKey] > y[sortKey]) return returnValues[0]
-    else return returnValues[1]
-  })
-}
+    if (x[sortKey] > y[sortKey]) return returnValues[0];
+    else return returnValues[1];
+  });
+};
 
 let read = (storePath, query = {}, options = {}) => {
   let defaultOptions = {
     descending: false
-  }
-  options = Object.assign(defaultOptions, options)
+  };
+  options = Object.assign(defaultOptions, options);
   if (query._id) {
-    return fs.readFile(`${storePath}/${query._id}.json`).then((data) => {
-      if (data == null || data.length < 1) return null
-      return JSON.parse(data)
+    return readJsonFile(`${storePath}/${query._id}.json`).then((data) => {
+      return data;
     }).catch((error) => {
-      return null
-    })
+      return null;
+    });
   } else {
     return fs.readdir(storePath).then((files) => {
-      let jsonFiles = files.filter(file => path.extname(file) === ".json")
-      let tmpFiles = files.filter(file => path.extname(file).indexOf(".tmp") === 0)
+      let jsonFiles = files.filter(file => path.extname(file) === ".json");
+      let tmpFiles = files.filter(file => path.extname(file).indexOf(".tmp") === 0);
       tmpFiles.forEach((file) => {
         fs.unlink(`${storePath}/${file}`).catch(() => {});
       });
       let readPromises = jsonFiles.map((file) => {
-        let _id = path.basename(file, ".json")
-        return read(storePath, {_id})
-      })
+        let _id = path.basename(file, ".json");
+        return read(storePath, {_id});
+      });
       return Promise.all(readPromises).then((objects) => {
-        objects = filterObjects(objects, query)
+        objects = filterObjects(objects, query);
         if (options.sort) {
-          let {descending} = options
-          objects = sortObjects(objects, options.sort, {descending})
+          let {descending} = options;
+          objects = sortObjects(objects, options.sort, {descending});
         }
-        return objects
-      })
+        return objects;
+      });
     }).catch((error) => {
-      return null
-    })
+      return null;
+    });
   }
-}
+};
 
 let write = (storePath, object = {}, options = {}) => {
-  if (Object.keys(object) < 1) return false
+  if (Object.keys(object) < 1) return false;
   if (object._id == null) {
-    object._id = generateDefaultId()
+    object._id = generateDefaultId();
   }
-  let file = `${storePath}/${object._id}.json`
+  let file = `${storePath}/${object._id}.json`;
   let write = () => {
     return writeJsonFile(file, object).then(() => {
-      return object
+      return object;
     }).catch((error) => {
-      return false
-    })
-  }
+      return false;
+    });
+  };
   if (options.mkdirp === true) {
-    return fs.mkdirp(storePath).then(write)
+    return fs.mkdirp(storePath).then(write);
   } else {
-    return write()
+    return write();
   }
-}
+};
 
 let remove = (storePath, query = {}) => {
   if (query._id) {
     return fs.unlink(`${storePath}/${query._id}.json`).then(() => {
-      return true
+      return true;
     }).catch((error) => {
-      return false
-    })
+      return false;
+    });
   } else {
     return fs.readdir(storePath).then((files) => {
       if (Object.keys(query).length < 1) {
         let unlinkPromises = files.map(file => {
-          return fs.unlink(`${storePath}/${file}`)
-        })
+          return fs.unlink(`${storePath}/${file}`);
+        });
         return Promise.all(unlinkPromises).then(() => {
-          return fs.rmdir(storePath).then(() => true)
-        })
+          return fs.rmdir(storePath).then(() => true);
+        });
       } else {
         return read(storePath, query).then((objects) => {
           let unlinkPromises = objects.map((object) => {
-            return fs.unlink(`${storePath}/${object._id}.json`)
-          })
-          return Promise.all(unlinkPromises).then(() => true)
-        })
+            return fs.unlink(`${storePath}/${object._id}.json`);
+          });
+          return Promise.all(unlinkPromises).then(() => true);
+        });
       }
     }).catch((error) => {
-      return false
-    })
+      return false;
+    });
   }
-}
+};
 
-module.exports = {read, write, remove}
+module.exports = {read, write, remove};
--- test/index.test.js
+++ test/index.test.js
@@ -1,111 +1,112 @@
-let fs = require("fs")
-let pify = require("pify")
-let rimraf = require("rimraf")
-let jsonDatastore = require("./../src/index")
-let datastorePath = `${process.cwd()}/tmp/datastore`
+let fs = require("fs");
+let pify = require("pify");
+let rimraf = require("rimraf");
+let jsonDatastore = require("./../src/index");
+let datastorePath = `${process.cwd()}/tmp/datastore`;
 
-fs = pify(fs)
-rimraf = pify(rimraf)
+fs = pify(fs);
+rimraf = pify(rimraf);
 
 beforeEach(() => {
-  return rimraf(datastorePath)
-})
+  return rimraf(datastorePath);
+});
 
 afterEach(() => {
-  return rimraf(datastorePath)
-})
+  return rimraf(datastorePath);
+});
 
 describe("read", () => {
   test("read from a non-exisiting path", async () => {
     let result = await jsonDatastore.read(datastorePath);
-    expect(result).toBe(null)
+    expect(result).toBe(null);
     result = await jsonDatastore.read(datastorePath, {_id: 1});
-    expect(result).toBe(null)
-  })
+    expect(result).toBe(null);
+  });
 
   test("read by id", async () => {
-    let data = {_id: 1, string: "hello"}
-    let options = {mkdirp: true}
-    await jsonDatastore.write(datastorePath, data, options)
-    let result = await jsonDatastore.read(datastorePath, {_id: 1})
-    expect(result).toEqual(data)
-  })
+    let data = {_id: 1, string: "hello"};
+    let options = {mkdirp: true};
+    await jsonDatastore.write(datastorePath, data, options);
+    let result = await jsonDatastore.read(datastorePath, {_id: 1});
+    expect(result).toEqual(data);
+  });
 
   test("read all", async () => {
-    let data1 = {_id: 1, string: "hello"}
-    let data2 = {_id: 2, string: "world"}
-    let options = {mkdirp: true}
+    let data1 = {_id: 1, string: "hello"};
+    let data2 = {_id: 2, string: "world"};
+    let options = {mkdirp: true};
     await Promise.all([
       jsonDatastore.write(datastorePath, data1, options),
       jsonDatastore.write(datastorePath, data2, options)
-    ])
-    let result = await jsonDatastore.read(datastorePath)
-    expect(result.length).toBe(2)
-  })
+    ]);
+    let result = await jsonDatastore.read(datastorePath);
+    expect(result).toHaveLength(2);
+  });
 });
 
 describe("write", () => {
   test("write to a non-exisiting path", async () => {
-    let data = {_id: 1, string: "hello"}
-    let result = await jsonDatastore.write(datastorePath, data)
-    expect(result).toBe(false)
-  })
+    let data = {_id: 1, string: "hello"};
+    let result = await jsonDatastore.write(datastorePath, data);
+    expect(result).toBe(false);
+  });
 
   test("write to an exisiting path", async () => {
-    let data = {_id: 1, string: "hello"}
-    await fs.mkdir(datastorePath)
-    let result = await jsonDatastore.write(datastorePath, data)
-    expect(result).toEqual(data)
-  })
+    let data = {_id: 1, string: "hello"};
+    await fs.mkdir(datastorePath);
+    let result = await jsonDatastore.write(datastorePath, data);
+    expect(result).toEqual(data);
+  });
 
   test("write with mkdirp", async () => {
-    let data = {_id: 1, string: "hello"}
-    let options = {mkdirp: true}
-    let result = await jsonDatastore.write(datastorePath, data, options)
-    expect(result).toEqual(data)
-  })
+    let data = {_id: 1, string: "hello"};
+    let options = {mkdirp: true};
+    let result = await jsonDatastore.write(datastorePath, data, options);
+    expect(result).toEqual(data);
+  });
 
   test("write to an exisiting file (update)", async () => {
-    let data = {_id: 1, string: "hello"}
-    let options = {mkdirp: true}
-    await jsonDatastore.write(datastorePath, data, options)
-    data.string = "hello-updated"
-    let writeResult = await jsonDatastore.write(datastorePath, data, options)
-    let readResult = await jsonDatastore.read(datastorePath)
-    expect(readResult.length).toBe(1)
-    expect(readResult[0]).toEqual(data)
-  })
+    let data = {_id: 1, string: "hello"};
+    let options = {mkdirp: true};
+    await jsonDatastore.write(datastorePath, data, options);
+    data.string = "hello-updated";
+    let writeResult = await jsonDatastore.write(datastorePath, data, options);
+    expect(writeResult).toEqual(data);
+    let readResult = await jsonDatastore.read(datastorePath);
+    expect(readResult).toHaveLength(1);
+    expect(readResult[0]).toEqual(data);
+  });
 });
 
 describe("remove", () => {
   test("remove a non-exisiting path", async () => {
-    let result = await jsonDatastore.remove(datastorePath)
-    expect(result).toBe(false)
-    result = await jsonDatastore.remove(datastorePath, {_id: 1})
-    expect(result).toBe(false)
-  })
+    let result = await jsonDatastore.remove(datastorePath);
+    expect(result).toBe(false);
+    result = await jsonDatastore.remove(datastorePath, {_id: 1});
+    expect(result).toBe(false);
+  });
 
   test("remove by id", async () => {
-    let data = {_id: 1, string: "hello"}
-    let options = {mkdirp: true}
-    await jsonDatastore.write(datastorePath, data, options)
-    let result = await jsonDatastore.remove(datastorePath, {_id: 1})
-    expect(result).toBe(true)
-    result = await jsonDatastore.read(datastorePath, {_id: 1})
-    expect(result).toBe(null)
-  })
+    let data = {_id: 1, string: "hello"};
+    let options = {mkdirp: true};
+    await jsonDatastore.write(datastorePath, data, options);
+    let result = await jsonDatastore.remove(datastorePath, {_id: 1});
+    expect(result).toBe(true);
+    result = await jsonDatastore.read(datastorePath, {_id: 1});
+    expect(result).toBe(null);
+  });
 
   test("remove all", async () => {
-    let data1 = {_id: 1, string: "hello"}
-    let data2 = {_id: 2, string: "world"}
-    let options = {mkdirp: true}
+    let data1 = {_id: 1, string: "hello"};
+    let data2 = {_id: 2, string: "world"};
+    let options = {mkdirp: true};
     await Promise.all([
       jsonDatastore.write(datastorePath, data1, options),
       jsonDatastore.write(datastorePath, data2, options)
-    ])
-    let result = await jsonDatastore.remove(datastorePath)
-    expect(result).toBe(true)
-    result = await jsonDatastore.read(datastorePath)
-    expect(result).toBe(null)
-  })
+    ]);
+    let result = await jsonDatastore.remove(datastorePath);
+    expect(result).toBe(true);
+    result = await jsonDatastore.read(datastorePath);
+    expect(result).toBe(null);
+  });
 });
